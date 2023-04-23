47de8bf6fce333abe8dec87521fc787b0dd18488
yudha <yamashitataiki123@gmail.com>
2023-04-23 22:52:13 +0700

    Test Core Features

---
+++ matcha-adventure/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Level0event_Event18_Act2(runtime, localVars)
+	{
+		console.log("Collide with patrol");
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
