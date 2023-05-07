c796c77bb75bce1de4ac635f8304d2c4d6d44050
yudha <yamashitataiki123@gmail.com>
2023-05-08 00:02:51 +0700

    Push level designs

--- matcha-adventure/scripts/project/scriptsInEvents.js
+++ matcha-adventure/scripts/project/scriptsInEvents.js
@@ -6,6 +6,21 @@ const scriptsInEvents = {
 	async Level0event_Event18_Act2(runtime, localVars)
 	{
 		console.log("Collide with patrol");
+	},
+
+	async Level1event_Event18_Act2(runtime, localVars)
+	{
+		console.log("Collide with patrol");
+	},
+
+	async Level2event_Event18_Act2(runtime, localVars)
+	{
+		console.log("Collide with patrol");
+	},
+
+	async Level3event_Event18_Act2(runtime, localVars)
+	{
+		console.log("Collide with patrol");
 	}
 
 };
